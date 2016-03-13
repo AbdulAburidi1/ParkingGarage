53b10f3bf6b80e24392b83b1b1ad78cde62a208f
AbdulAburidi1 <abdalmenem.aburidi@utoledo.edu>
2016-03-13 15:07:02 -0400

    Update ParkingGarage.java

--- ParkingGarage.java
+++ ParkingGarage.java
@@ -1,1 +1,75 @@
-import java.util.Scanner;import java.text.DecimalFormat;public class ParkingGarage {    public static void main(String[] args) {        Scanner input = new Scanner(System.in);        /* Abdalmenem Aburidi         1510 - Project 4 Program (2)         A parking garage has 5 customers daily.         The garage charges a $5.00 minimum fee to park up to two hours.         The garage charges an additional $1.00 per hour for each hour         (or part of an hour) over two hours.  The maximum charge for any given         day is $12.00.  All cars are gone by midnight.  Write a program to         calculate and print a summary of the charges for a day.  For input         the program will read the hours of usage for each of the 5 cars.  The program         will print the result, as shown below.         */        double car1, car2, car3, car4, car5, Total1, hour, i, Total2, Total3, Total4, Total5;        double totalhours, totalcharge, H;        String pattern = "00.0";        String pattern1 = "$00.00";        DecimalFormat df, df1;        df = new DecimalFormat(pattern);        df1 = new DecimalFormat(pattern1);        System.out.print("Enter the hours parked for car 1: ");        car1 = input.nextDouble();        System.out.print("Enter the hours parked for car 2: ");        car2 = input.nextDouble();        System.out.print("Enter the hours parked for car 3: ");        car3 = input.nextDouble();        System.out.print("Enter the hours parked for car 4: ");        car4 = input.nextDouble();        System.out.print("Enter the hours parked for car 5: ");        car5 = input.nextDouble();        hour = 2;        i = 5;        H = 12;        {            if (car1 > hour)                Total1 = Math.ceil(car1) + i - hour;            else                Total1 = i;            if (car2 > hour)                Total2 = Math.ceil(car2) + i - hour;            else                Total2 = i;            if (car3 > hour)                Total3 = Math.ceil(car3) + i - hour;            else                Total3 = i;            if (car4 > hour)                Total4 = Math.ceil(car4) + i - hour;            else                Total4 = i;            if (car5 > hour)                Total5 = Math.ceil(car5) + i - hour;            else                Total5 = i;        }        if (Total1 > H)            Total1 = 12;        if (Total2 > H)            Total2 = 12;        if (Total3 > H)            Total3 = 12;        if (Total4 > H)            Total4 = 12;        if (Total5 > H)            Total5 = 12;        totalhours = car1 + car2 + car3 + car4 + car5;        totalcharge = Total1 + Total2 + Total3 + Total4 + Total5;		        System.out.println("");        System.out.println("Total Hours " + df.format(totalhours));        System.out.println("Total Charge  " + df1.format(totalcharge));    }}
+import java.util.Scanner;
+import java.text.DecimalFormat;
+public class ParkingGarage {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        /* Abdalmenem Aburidi
+         1510 - Project 4 Program (2)
+         A parking garage has 5 customers daily.
+         The garage charges a $5.00 minimum fee to park up to two hours.
+         The garage charges an additional $1.00 per hour for each hour
+         (or part of an hour) over two hours.  The maximum charge for any given
+         day is $12.00.  All cars are gone by midnight.  Write a program to
+         calculate and print a summary of the charges for a day.  For input
+         the program will read the hours of usage for each of the 5 cars.  The program
+         will print the result, as shown below.
+         */
+        double car1, car2, car3, car4, car5, Total1, FirstHr, MinFee, Total2, Total3, Total4, Total5;
+        double totalhours, totalcharge, MaxCharge;
+        String pattern = "00.0";
+        String pattern1 = "$00.00";
+        DecimalFormat df, df1;
+        df = new DecimalFormat(pattern);
+        df1 = new DecimalFormat(pattern1);
+        System.out.print("Enter the hours parked for car 1: ");
+        car1 = input.nextDouble();
+        System.out.print("Enter the hours parked for car 2: ");
+        car2 = input.nextDouble();
+        System.out.print("Enter the hours parked for car 3: ");
+        car3 = input.nextDouble();
+        System.out.print("Enter the hours parked for car 4: ");
+        car4 = input.nextDouble();
+        System.out.print("Enter the hours parked for car 5: ");
+        car5 = input.nextDouble();
+        FirstHr = 2;
+        MinFee = 5;
+        MaxCharge = 12;
+        {
+            if (car1 > FirstHr)
+                Total1 = Math.ceil(car1) + MinFee - FirstHr;
+            else
+                Total1 = MinFee;
+            if (car2 > FirstHr)
+                Total2 = Math.ceil(car2) + MinFee - FirstHr;
+            else
+                Total2 = MinFee;
+            if (car3 > FirstHr)
+                Total3 = Math.ceil(car3) + MinFee - FirstHr;
+            else
+                Total3 = MinFee;
+            if (car4 > FirstHr)
+                Total4 = Math.ceil(car4) + MinFee - FirstHr;
+            else
+                Total4 = MinFee;
+            if (car5 > FirstHr)
+                Total5 = Math.ceil(car5) + MinFee - FirstHr;
+            else
+                Total5 = MinFee;
+        }
+        if (Total1 > MaxCharge)
+            Total1 = 12;
+        if (Total2 > MaxCharge)
+            Total2 = 12;
+        if (Total3 > MaxCharge)
+            Total3 = 12;
+        if (Total4 > MaxCharge)
+            Total4 = 12;
+        if (Total5 > MaxCharge)
+            Total5 = 12;
+        totalhours = car1 + car2 + car3 + car4 + car5;
+        totalcharge = Total1 + Total2 + Total3 + Total4 + Total5;		
+        System.out.println("");
+        System.out.println("Total Hours " + df.format(totalhours));
+        System.out.println("Total Charge  " + df1.format(totalcharge));
+    }
+}
